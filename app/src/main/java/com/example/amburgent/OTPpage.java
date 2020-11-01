0795b746a53adf9985bbe28f19f3cf5ec2ac3ae4
Proma Chowdhury <48786787+PromaChow@users.noreply.github.com>
2020-11-01 14:38:40 +0600

    com
    commit

---
+++ app/src/main/java/com/example/amburgent/OTPpage.java
@@ -0,0 +1,14 @@
+package com.example.amburgent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OTPpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otppage);
+    }
+}
